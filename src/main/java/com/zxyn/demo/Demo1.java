6d16f9223a9df86dfced84b509795d374a0891c1
hss <398690424@qq.com>
2020-04-13 10:58:40 +0800

    测试代码减少

--- src/main/java/com/zxyn/demo/Demo1.java
+++ src/main/java/com/zxyn/demo/Demo1.java
@@ -17,18 +17,6 @@ public class Demo1 {
         String age = request.getParameter("age");
         String ip = request.getRemoteAddr();
         System.out.println("ip>>>>>>"+ip);
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
-        System.out.println("====================");
         return name+","+age;
     }
 
